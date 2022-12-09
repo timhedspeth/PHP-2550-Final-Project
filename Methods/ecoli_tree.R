dab47c8fc3d3309e516f4af3925299bce2dc4594
Anthony-Girard <46202697+Anthony-Girard@users.noreply.github.com>
2022-12-09 10:49:09 -0500

    Add files via upload

---
+++ Methods/ecoli_tree.R
@@ -0,0 +1,227 @@
+library(kableExtra)
+library(tidyverse)
+library(ggpubr)
+library(mice) 
+library(glmnet)  
+library(bestglm)
+library(pROC)
+library(DescTools)
+library(caret)
+library(kableExtra)
+library(ggpubr)
+library(rpart)
+
+ecoli <- read.csv("~/Brown University/PHP_2550/Data/final_ecoli.csv")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~#
+## Define the functions ## 
+#~~~~~~~~~~~~~~~~~~~~~~~~#
+
+add_outbreak <- function(dataset, numbercases = 10, similarity = 7){
+  #' This function will create a binary variable to identify if a case was part of a user defined outbreak
+  #' 
+  #'@param data, the data frame of interest, must contain 
+  #'@param numbercases, the number of cases in your outbreak 
+  #'@param similarity, how closely related do you want you want the strains to be 
+  
+  dataset <- as.data.frame(dataset)
+  
+  
+  # Try to catch an inappropriate data frame 
+  if(!("month" %in% names(dataset) | "year" %in% names(dataset) | 
+       "region" %in% names(dataset) | "Min.same" %in% names(dataset))){
+    warning("It appears you don't have the proper columns, please make sure you
+            have month, year, region, and Min.same as columns")
+  }
+  
+  # Find the number of cases in an outbreak   
+  data_subset <- dataset %>%
+    filter(!is.na(month) & (region  != "USA, General")) %>% 
+    group_by(year, month, SNP.cluster) %>% 
+    summarize(#mean1 = round(mean(Min.same, na.rm = T),2), 
+      #sd1 = round(sd(Min.same, na.rm = T),2),
+      num = n())  %>% 
+    ungroup() %>% 
+    # Substantial number of cases that are very closely related 
+    filter(num > numbercases)  %>% 
+    mutate(outbreak = 1)
+  #pivot_wider(names_from = region, values_from = c(mean1, sd1))
+  
+  # Join inoutbreak information
+  dataset <- left_join(dataset, data_subset, by= c("year", "month", "SNP.cluster")) %>%
+    select(-c(num))
+  dataset$outbreak[is.na(dataset$outbreak)] <- 0
+  
+  # What do we return? 
+  if(sum(dataset$outbreak ==1) == 0){
+    return("There appear to be no outbreaks in this data based on your paramters")
+  } else{
+    return(dataset)
+  }
+  
+  
+}
+
+
+#~~~~~~~~~~~~~~~~~~#
+## Lasso Function ## 
+#~~~~~~~~~~~~~~~~~~# 
+
+# This function was adapted from a code provided by Alice Paul, PhD, the 
+# instructor for  this course 
+
+lasso <- function(df,numtimes, prob = .3) { 
+  #' Runs 10-fold CV for lasso and returns corresponding coefficients 
+  #' @param df, data set
+  #' @return coef, coefficients for minimum cv error
+  
+  # Matrix form for ordered variables 
+  x.ord <- model.matrix(outbreak~., data = df)[,-1] 
+  y.ord <- df$outbreak
+  
+  # Generate folds
+  k <- numtimes 
+  #set.seed(1) # consistent seeds between imputed data sets
+  folds <- as.numeric(df$month)
+  
+  
+  #  This function will auto do weighting 
+  
+  prop0 <-sum(df$outbreak == 0)/nrow(df)
+  prop1 <- sum(df$outbreak == 1)/nrow(df)
+  modweight <- ifelse(df$outbreak == 0, 
+                      1/prop0, 
+                      1/prop1)
+  
+  # Lasso model
+  #if(prop0 > prob & prop1 > prob){
+  #lasso_mod <- cv.glmnet(x.ord, y.ord, nfolds = numtimes, foldid = folds, 
+  #                      alpha = 1, family = "binomial") 
+  #} else{
+  lasso_mod <- cv.glmnet(x.ord, y.ord, nfolds = 12, foldid = folds, 
+                         alpha = 1, family = "binomial", weights = modweight) 
+  #}
+  
+  # Get coefficients 
+  coef <- coef(lasso_mod, lambda = lasso_mod$lambda.min) 
+  return(coef) 
+} 
+
+
+
+## We will do a complete case analysis ##
+
+ecoli <- ecoli %>% filter(!is.na(month) & !is.na(year) & 
+                            !is.na(SNP.cluster)  & !is.na(Strain)) %>%
+  select(-c(day, Min.same))
+
+
+
+# We will add an outbreak to the model 
+ecoli <- add_outbreak(ecoli, 15)
+#lapply(ecoli, function(x) sum(is.na(x))/length(x))
+
+#ecoli <- add_outbreak(ecoli)
+
+# Reclean the data 
+
+SNP_df <- as.data.frame(table(as.factor(ecoli$SNP.cluster)))
+snp_included <- SNP_df %>% filter(Freq > 100)
+levels_snp <- as.vector(snp_included$Var1)
+ecoli$SNP.cluster <- case_when(ecoli$SNP.cluster %in% levels_snp ~ ecoli$SNP.cluster, 
+                               is.na(ecoli$SNP.cluster) ~ ecoli$SNP.cluster,
+                               !(ecoli$SNP.cluster %in% levels_snp) ~ "Other")
+
+
+AMR_df <- as.data.frame(table(as.factor(ecoli$AMR.genotypes)))
+AMR_included <- AMR_df %>% filter(Freq > 300)
+levels_AMR <- as.vector(AMR_included$Var1)
+ecoli$AMR.genotypes <- case_when(ecoli$AMR.genotypes %in% levels_AMR ~ ecoli$AMR.genotypes, 
+                                 is.na(ecoli$AMR.genotypes) ~ ecoli$AMR.genotypes,
+                                 !(ecoli$AMR.genotypes %in% levels_AMR) ~ "Other")
+
+
+
+ecoli <- ecoli %>% select(-c(Location))
+
+ecoli_train <- ecoli %>% filter(year != 2021 & year !=  2019)
+ecoli_test <- ecoli  %>%  filter(year == 2021 | year ==  2019 )
+table(ecoli_test$outbreak)
+
+ecoli_train <- ecoli_train %>% select(-c(year))
+ecoli_test <- ecoli_test %>% select(-c(year))
+
+
+ecoli_train[] <- lapply(ecoli_train, function(x){return(as.factor(x))})
+ecoli_test[] <- lapply(ecoli_test, function(x){return(as.factor(x))})
+
+
+ecoli_test$Strain <- factor(ecoli_test$Strain, 
+                            levels = c("Ecoli",
+                                       "O-:H28",
+                                       "O145:H28",
+                                       "O157:H7",
+                                       "Other", 
+                                       "ST131"))
+ecoli_test$Strain <- relevel(ecoli_test$Strain, "Ecoli")
+
+ecoli_test$SNP.cluster <- factor(ecoli_test$SNP.cluster, 
+                                 levels = c("Other", 
+                                            "PDS000053707.4",
+                                            "PDS000066932.4",
+                                            "PDS000117186.31"))
+
+tree_ecoli <- rpart(outbreak ~ ., data = ecoli_train, method = "class", control=rpart.control(minsplit = 20, minbucket = 30,cp =.01)) 
+
+plot(tree_ecoli,  
+     main = "Intial tree model",  
+     sub = "Figure 2. Intial tree model, prior to pruning")  
+text(tree_ecoli) 
+
+plotcp(tree_ecoli, sub = "Figure 3. How x error changes based on complexity") 
+printcp(tree_ecoli) 
+
+preds = predict(tree_ecoli, ecoli_train, type = "class")
+sensitivity(preds, ecoli_train$outbreak)
+specificity(preds, ecoli_train$outbreak)
+
+# Predict with Tree
+predict_ecoli_tree <- as.data.frame(predict(tree_ecoli, ecoli_train))
+names(predict_ecoli_tree) <- c("prob_tree")
+ecoli_append <- cbind(ecoli_train, predict_ecoli_tree)
+
+# ROC Analysis
+ecoli_acc <- roc(outbreak ~ prob_tree, data =ecoli_append)
+#AUC
+ecoli_AUC = round(auc(ecoli_acc), 2)
+
+# Brier Score
+ecoli_Brscr = round(sum((predict_ecoli_tree[,2] - (as.numeric(ecoli_train$outbreak) - 1))^2)/length(ecoli_train$outbreak),2)
+
+ggroc(ecoli_acc) + ggtitle('E. Coli Classification Tree ROC Curve') + 
+  annotate("text", x = .91, y = .95, label = paste0('AUC = ', ecoli_AUC), color = 'red') +
+  annotate("text", x = .85, y = .9, label = paste0('Brier Score = ', ecoli_Brscr), color = 'red')
+
+
+# Predict on Test Set
+pred_test = predict(tree_ecoli, newdata = ecoli_test, type = "class")
+sensitivity(pred_test, ecoli_test$outbreak)
+specificity(pred_test, ecoli_test$outbreak)
+
+# Predict with Tree
+predict_test_tree <- as.data.frame(predict(tree_ecoli, ecoli_test))
+names(predict_test_tree) <- c("prob_tree")
+ecoli_test_append <- cbind(ecoli_test, predict_test_tree)
+
+# ROC Analysis
+ecoli_test_acc <- roc(outbreak ~ prob_tree, data =ecoli_test_append)
+#AUC
+ecoli_test_AUC = round(auc(ecoli_test_acc), 2)
+
+# Brier Score
+ecoli_test_Brscr = round(sum((predict_test_tree[,2] - (as.numeric(ecoli_test$outbreak) - 1))^2)/length(ecoli_test$outbreak),2)
+
+ggroc(ecoli_test_acc) + ggtitle('E. Coli Classification Tree ROC Curve') + 
+  annotate("text", x = .91, y = .95, label = paste0('AUC = ', ecoli_test_AUC), color = 'red') +
+  annotate("text", x = .85, y = .9, label = paste0('Brier Score = ', ecoli_test_Brscr), color = 'red')
+
